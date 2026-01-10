36f313dcc03283e25feedaf795207d897e2aa65d
Carnage098 <polairebleu7@gmail.com>
2026-01-10 01:10:44 +0100

    Add Yu-Gi-Oh! card list to cards.py
    Added a comprehensive list of Yu-Gi-Oh! cards to cards.py.

--- cards.py
+++ cards.py
@@ -1,1 +1,348 @@
-
+Magicien Sombre
+Magicienne des Ténèbres
+Dragon Blanc aux Yeux Bleus
+Dragon Noir aux Yeux Rouges
+Exodia l’Interdit
+Bras Gauche de l’Interdit
+Bras Droit de l’Interdit
+Jambe Gauche de l’Interdit
+Jambe Droite de l’Interdit
+Kuriboh
+Assistant du Temps
+Crâne Invoqué
+Buster Blader
+Gaïa le Chevalier Féroce
+Soldat du Lustre Noir
+Soldat du Lustre Noir – Émissaire du Commencement
+Dragon Empereur du Chaos – Émissaire de la Fin
+Jinzo
+Obélisque le Tourmenteur
+Slifer le Dragon Céleste
+Le Dragon Ailé de Râ
+Le Dragon Ailé de Râ – Mode Sphère
+Le Dragon Ailé de Râ – Immortel du Phénix
+Force de Miroir
+Raigeki
+Monster Reborn
+Pot de Cupidité
+Polymérisation
+Trou Noir
+Changement de Cœur
+Épées de Révélation de la Lumière
+Plumeau de Dame Harpie
+Dragoon Noir aux Yeux Rouges
+Dragon Ultime aux Yeux Bleus
+Dragon Blanc Alternatif aux Yeux Bleus
+Dragon Jet aux Yeux Bleus
+Dragon Abyssal aux Yeux Bleus
+Dragon Argenté aux Yeux Azur
+Dragon Spirituel aux Yeux Bleus
+Dragon Poussière d’Étoile
+Dragon Étoile Filante
+Dragon Rouge Archdémon
+Dragon Rouge Archdémon Écarlate
+Dragon Rouge Archdémon Roi du Fléau
+Dragon Rouge Archdémon Fléau
+Dragon Synchro Aile Cristalline
+Dragon Synchro Aile Claire
+Dragon Pendule aux Yeux Impairs
+Dragon Rébellion aux Yeux Impairs
+Dragon Vortex aux Yeux Impairs
+Dragon Suprême Z-ARC
+Numéro 39 : Utopie
+Numéro 62 : Dragon Photon Prime aux Yeux Galactiques
+Dragon Photon aux Yeux Galactiques
+Dragon Cipher aux Yeux Galactiques
+Dragon Photon Armure Complète
+Dragon Cipher à Lame
+Dragon Cyber
+Noyau Dragon Cyber
+Cœur Dragon Cyber
+Dragon Cyber Infini
+Dragon Cyber Nova
+Dragon Cyber Jumeau
+Dragon Cyber Ultime
+HÉROS Élémentaire Néos
+HÉROS Élémentaire Stratos
+HÉROS Élémentaire Brume de l’Ombre
+HÉROS Élémentaire Soldat Liquide
+HÉROS Élémentaire Flame Wingman
+HÉROS Élémentaire Géant du Tonnerre
+HÉROS Élémentaire Lever du Soleil
+HÉROS Masqué Loi des Ténèbres
+HÉROS Masqué Anki
+HÉROS Masqué Acide
+HÉROS de la Destinée – Malveillant
+HÉROS de la Destinée – Plasma
+HÉROS Vision Faris
+HÉROS Vision Increase
+HÉROS Vision Vyon
+Accèscode Bavard
+Décodeur Bavard
+Dragon Pare-Feu
+Linkuriboh
+Anima Relinquished
+Apollousa, Arc de la Déesse
+I:P Masquerena
+Chevalier du Cauchemar Phénix
+Chevalier du Cauchemar Licorne
+Chevalier du Cauchemar Cérbère
+Chevalier du Cauchemar Gryphon
+Dragon Borrelcode
+Dragon Borrelcharge
+Dragon Borrelend
+Dragon Topologique Zeroboros
+Dragon Topologique Bomber
+Floraison de Cendres & Joyeux Printemps
+Veilleur d’Effet
+Nibiru, l’Être Primitif
+Maxx “C”
+Imperméance Infinie
+Ogre Fantôme & Lapin des Neiges
+Belle Fantôme & Manoir Hanté
+Pleureuse Fantôme & Clair de Lune
+Désigné par la Tombe
+Désignateur de l’Exclusion
+Super Polymérisation
+Fusion du Destin
+Fusion Marquée
+Albaz le Déchu
+Dragon Albion le Marqué
+Dragon Lubellion le Calcinant
+Dragon Mirrorjade à la Lame de Glace
+Dragon Titaniklad des Cendres
+Aluber le Bouffon de Despia
+Dragon Mascarade de la Flamme Ardente
+Âme-Épée de Mo Ye
+Stratège Âme-Épée Longyuan
+Âme-Épée de Taia
+Grand Maître Âme-Épée – Chixiao
+Souverain Suprême Âme-Épée – Chengying
+Ecclésia l’Incomparable, la Vertueuse
+Esprit Tenyi – Ashuna
+Esprit Tenyi – Vishuda
+Esprit Tenyi – Adhara
+Esprit Tenyi – Shthana
+Esprit Tenyi – Mapura
+Baxia, Clarté du Yang Zing
+Yazi, Mal du Yang Zing
+Chaofeng, Fantôme du Yang Zing
+As du Ciel – Raye
+As du Ciel – Roze
+As du Ciel – Kagari
+As du Ciel – Shizuku
+As du Ciel – Hayate
+As du Ciel – Kaina
+Mobilisation As du Ciel – Engage !
+Mécha As du Ciel – Ancre Veuve
+Mécha As du Ciel – Canon Requin
+Modules Mécha As du Ciel – Multirôle
+Tearlaments Reinoheart
+Tearlaments Scheiren
+Tearlaments Havnis
+Tearlaments Merrli
+Tearlaments Kitkallos
+Tearlaments Rulkallos
+Planète Primordiale Perlereino
+Tearlaments Kaleido-Cœur
+Spright Bleu
+Spright Jet
+Spright Rouge
+Spright Carotte
+Démarrage Spright
+Spright Gigantesque
+Spright Elfe
+Spright Sprind
+Kashtira Fenrir
+Kashtira Licorne
+Kashtira Ogre
+Kashtira Riseheart
+Kashtira Shangri-Ira
+Kashtira Arise-Heart
+Planète Pressurisée Wraitsoth
+Dragon du Tonnerre
+Dragon du Tonnerre Obscur
+Dragon du Tonnerre Faucon
+Dragon du Tonnerre Rugissant
+Dragon du Tonnerre Matrice
+Dragon du Tonnerre Colosse
+Dragon du Tonnerre Titan
+Danger ! Nessie !
+Danger ! Jackalope !
+Danger ! Tsuchinoko !
+Danger ! Bigfoot !
+Danger ! Thunderbird !
+Danger ! Ogopogo !
+Salamangreat Gazelle
+Salamangreat Spinny
+Salamangreat Foxy
+Salamangreat Jaguar Féroce
+Salamangreat Balelynx
+Salamangreat Loup de Soleil
+Salamangreat Heatleo
+Salamangreat Miragestallio
+Mathmech Circulaire
+Mathmech Sigma
+Mathmech Diamètre
+Mathmech Addition
+Mathmech Soustraction
+Primathmech Alembertian
+Geomathmech Sigma Final
+Superfactorial Mathmech
+Seigneur Doré Eldlich
+Élixir Doré Écarlate
+Conquistador de la Terre Dorée
+Huaquero de la Terre Dorée
+Drain de Compétence
+Il ne peut y en avoir qu’un
+Match Gozen
+Rivalité des Seigneurs de Guerre
+Décalage Dimensionnel
+Égalité Parfaite
+Tempête de Foudre
+Talent des Trois Tactiques
+Poussée des Trois Tactiques
+Pot de la Prospérité
+Pot de l’Extravagance
+Pot de Dualité
+Gouttelette Interdite
+Livre de la Lune
+Livre de l’Éclipse
+Cyclone Cosmique
+Tornades Jumelles
+Plus de Dirigeant des Ténèbres
+Lien Araignée
+I:P Masquerena
+Déesse du Monde Souterrain
+Dragon Chargeborrelle Sauvage
+Dragon du Chaos Levianeer
+Dragon Noir Collapsépent
+Dragon Blanc Wyverburster
+Espace du Chaos
+Chaos Valkyria
+Créateur du Chaos
+Marqué en Rouge
+Marqué Perdu
+Bannissement Marqué
+Tragédie Despia
+Comédie Despia
+Ad Libitum de Despia
+Dramaturge de Despia
+Dragon Albion le Sanctifire
+Dragon du Crépuscule Granguignol
+Quem la Guidante, la Vertueuse
+Cartesia la Flamme Ardente, la Vertueuse
+Fusion aux Yeux Rouges
+Dragon Noir Métallique aux Yeux Rouges
+Dragon Métal Flare aux Yeux Rouges
+Dragon Noir Alternatif aux Yeux Rouges
+Magicien Sombre le Chevalier Dragon
+Âmes des Magiciens
+Illusion du Chaos
+Timaeus le Dragon Uni
+Serviteur de l’Âme
+Secrets de la Magie Noire
+Dragon Lien
+Traceur Rokket
+Rechargeur Rokket
+Synchroniseur Rokket
+Dragon Absorouteur
+Dragon Noctovision
+Starliege Seyfert
+Dragon Chaos Règle le Magique
+Dragon Chargeborrelle
+Dragon Épéeborrelle
+Dragon Chargeborrelle Sauvage
+Sceau Hiératique des Sphères Célestes
+Dragon Frappeur
+Gardien Dragon Pisty
+Gardien Dragon Elpy
+Dragon Pendule Arc aux Yeux Impairs
+Dragon Révolution aux Yeux Impairs
+Dragon Absolu aux Yeux Impairs
+Dragon Rafale aux Yeux Impairs
+Performapal Joker à Crânecrobat
+Performapal Singe de Combat
+Sorcier Astrographe
+Sorcier Chronographe
+Porte du Roi Suprême Zéro
+Porte du Roi Suprême Infini
+Endymion, le Puissant Maître de la Magie
+Serviteur d’Endymion
+Bête Mythique Roi Jackal
+Bête Mythique Maître Cerbère
+Electrumite Métalfoes Lourd
+Au-Delà du Pendule
+Baronne de Fleur
+Dragon Étoile Accélérée
+Synchroniseur Junk
+Synchroniseur Jet
+Réglage
+Synchroniseur de Formule
+Crystron Halqifibrax
+Bête Naturia
+Barkion Naturia
+Divinatrice de l’Héraut
+Héraut de l’Orange Lumière
+Héraut de l’Arc de Lumière
+Drytron Alpha Thuban
+Drytron Zeta Aldhibah
+Drytron Mu Beta Fafnir
+Ange Cyber Benten
+Eva
+PSY-Framegear Gamma
+PSY-Framegear Driver
+PSY-Framelord Oméga
+Monde Virtuel Mai-Hime – Lulu
+Monde Virtuel Roshi – Laolao
+Monde Virtuel Kyubi – Shenshen
+Porte du Monde Virtuel – Qinglong
+Porte du Monde Virtuel – Chuche
+Zoodiaque Ratpier
+Zoodiaque Whiptail
+Zoodiaque Thoroughblade
+Zoodiaque Drident
+Arsenal Divin AA-ZEUS – Tonnerre du Ciel
+Tri-Brigade Fraktall
+Tri-Brigade Kitt
+Tri-Brigade Nervall
+Tri-Brigade Shuraig le Mauvais Présage
+Lyrilusc – Moineau Cobalt
+Lyrilusc – Fauvette Turquoise
+Lyrilusc – Robin Ensemblue
+Floowandereeze & Robina
+Floowandereeze & Eglen
+Floowandereeze & Empen
+Floowandereeze & Snowl
+Albaz le Cendré
+Linkuriboh
+Anima Relinquished
+Super Grenouille
+Requin Bahamut
+Plongeuse des Profondeurs
+Dragoons Atlantes
+Mermail Abyssmegalo
+Mermail Abyssgaïos
+Grenouille Permutable
+Ronintoadin
+Grenouille Dupliquée
+Démolisseurs Spright
+Explosion Gamma Spright
+Cri Tearlaments
+Battement de Cœur Tearlaments
+Réprobation Tearlaments
+Naissance Kashtira
+Préparatifs Kashtira
+Planète Pressurisée Wraitsoth
+Égalité Parfaite
+Trou Noir
+Raigeki
+Plumeau de Dame Harpie
+Monster Reborn
+Désigné par la Tombe
+Imperméance Infinie
+Floraison de Cendres & Joyeux Printemps
+Veilleur d’Effet
+Gouttelette Interdite
+Talent des Trois Tactiques
+Poussée des Trois Tactiques
